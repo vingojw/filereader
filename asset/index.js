8d326a18fecd056f0fa6b99c54d563e95259b02f
vingojw <vingojw@gmail.com>
2016-01-12 21:28:44 +0800

    u

--- asset/index.js
+++ asset/index.js
@@ -60,10 +60,10 @@ new Vue({
     },
     ready:function(){
         var _this = this;
-        var gif= 'http://localhost:8080/asset/222.gif';
+        var gif= 'http://localhost:8080/asset/333.gif';
         pixel.parse(gif).then(function(images){
 
-          console.log(images);
+          console.log(images); 
           for (var i = 0; i < images.length; i++) {
             var cvs = document.createElement('canvas');
             var ctx=cvs.getContext("2d");
@@ -72,20 +72,27 @@ new Vue({
             var d = images[i];
             cvs.width = w;
             cvs.height = h;
-            //ctx.drawImage(d,w,h);
-            debugger;
-            // var imgData = ctx.createImageData(w,h);
-            // for (var i=0;i<d.data.length;i+=4){
-            //     imgData.data[i+0]=d.data[i+0];
-            //     imgData.data[i+1]=d.data[i+1];
-            //     imgData.data[i+2]=d.data[i+2];
-            //     imgData.data[i+3]=d.data[i+3];
-            // }
-            ctx.putImageData(d,w,h);
+            //ctx.drawImage(d,w,h);  
+            var imgData = ctx.createImageData(w,h);
+            for (var j=0;j<d.data.length;j+=4){
+                imgData.data[j+0]=d.data[j+0];
+                imgData.data[j+1]=d.data[j+1];
+                imgData.data[j+2]=d.data[j+2];
+                imgData.data[j+3]=d.data[j+3];
+            }
+            //ctx.putImageData(d,w,h);
+            /*
+                putImageData  有7个参数 
+                第一个 是 imageData
+                2、3分别是 图像的坐标
+                4、5要画到画布的坐标
+                6、7则是画的宽度
+             */
+            ctx.putImageData(d,0,0,0,0,w,h); 
             _this.$els.canvas.appendChild(cvs);
           };
-          console.log(images.loopCount); // 0(Infinite)
-          console.log(images[0]);
+          //console.log(images.loopCount); // 0(Infinite)
+          //console.log(images[0]);
         });
     }
 });
